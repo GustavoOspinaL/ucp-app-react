b9afb3ff91fffee2e94b59427b081d7d2e2adc70
GustavoOspinaL <tavolopez.dev@gmail.com>
2025-05-26 23:25:29 -0500

    Add parallel test 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('Test que falla', () => {
+    expect(true).toBe(false);
+});
+
 /* test('renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/¡Universidad Católica de Pereira !/i);
     expect(linkElement).toBeInTheDocument();
 }); */
-
-test('Test que falla', () => {
-    expect(true).toBe(false);
-});
